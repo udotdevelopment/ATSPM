208aac74184847a435cd273b2b1d9d5ecf105128
Shane Johnson <shanejohnson@utah.gov>
2017-11-13 15:49:53 -0700

    Plan factory changes

---
+++ MOE.Common/Business/PlanBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOE.Common.Business
+{
+    public class PlanBase
+    {
+        public DateTime PlanStart { get; set; }
+        public DateTime PlanEnd { get; set; }
+        public int PlanNumber { get; set; }
+    }
+}
