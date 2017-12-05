d0ad294a89d1f5ebb72fa378a869b4b621712111
Shane Johnson <shanejohnson@utah.gov>
2017-12-05 11:18:57 -0700

    Added the repository files

---
+++ MOE.Common/Models/Repositories/AggregationMetricTypeRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOE.Common.Models.Repositories
+{
+    public class AggregationMetricTypeRepository : IAggregationMetricTypeRepository
+    {
+        Models.SPM db = new SPM();
+
+
+        public List<Models.AggregationMetricType> GetAllMetrics()
+        {
+            List<Models.AggregationMetricType> results = (from r in db.AggregationMetricTypes
+                                           select r).ToList();
+            return results;
+        }
+
+
+
+        public List<AggregationMetricType> GetAllToDisplayMetrics()
+        {
+            List<Models.AggregationMetricType> results = (from r in db.AggregationMetricTypes
+                                               where r.ShowOnWebsite == true
+                                               select r).ToList();
+            return results;
+        }
+
+
+
+        public List<AggregationMetricType> GetMetricsByIDs(List<int> metricIDs)
+        {
+            return db.AggregationMetricTypes.Where(x => metricIDs.Contains(x.MetricID)).ToList();            
+        }
+
+        public AggregationMetricType GetMetricsByID(int metricID)
+        {
+            var metricType = db.AggregationMetricTypes.Find(metricID);
+            return metricType;
+        }
+
+        
+    }
+}
---
+++ MOE.Common/Models/Repositories/AggregationMetricTypeRepositoryFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOE.Common.Models.Repositories
+{
+    public class AggregationMetricTypeRepositoryFactory
+    {
+        private static IAggregationMetricTypeRepository aggregationMetricTypeRepository;
+
+        public static IAggregationMetricTypeRepository Create()
+        {
+            if (aggregationMetricTypeRepository != null)
+            {
+                return aggregationMetricTypeRepository;
+            }
+            return new AggregationMetricTypeRepository();
+        }
+
+        public static void SetMetricsRepository(IAggregationMetricTypeRepository newRepository)
+        {
+            aggregationMetricTypeRepository = newRepository;
+        }
+    }
+}
