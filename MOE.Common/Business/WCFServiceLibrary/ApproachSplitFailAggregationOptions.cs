35430718861dc95bdc0554ddb56cabd4b677464b
Shane Johnson <shanejohnson@utah.gov>
2017-12-14 00:07:44 -0700

    Chnages to the chart factory to support aggregation reports

--- MOE.Common/Business/WCFServiceLibrary/ApproachSplitFailAggregationOptions.cs
+++ MOE.Common/Business/WCFServiceLibrary/ApproachSplitFailAggregationOptions.cs
@@ -28,20 +28,7 @@ namespace MOE.Common.Business.WCFServiceLibrary
                     DateTime reportStart = StartDate;
 
 
-                    //var sum = aggregations.Sum(r => r.ForceOffs);
 
-                    //while (reportStart < EndDate)
-                    //{
-                    //    var binValues = (from r in aggregations
-                    //        where r.BinStartTime <= reportStart && r.BinStartTime > reportStart.AddMinutes(BinSize)
-                    //        select r).ToList();
-
-                    //    int sfSum = binValues.Sum(f => f.SplitFailures);
-
-                    //    splitFailSeries.Points.AddXY(sfSum, reportStart);
-
-                    //    reportStart = reportStart.AddMinutes(BinSize);
-                    //}
 
                     
 
---
+++ MOE.CommonTests/MOE/CommonTests/Models/InMemoryApproachSplitFailAggregationRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MOE.Common.Models;
+using MOE.Common.Models.Repositories;
+
+namespace MOE.CommonTests.Models
+{
+    public class InMemoryApproachSplitFailAggregationRepository: IApproachSplitFailAggregationRepository
+    {
+        private InMemoryMOEDatabase _db;
+
+        public InMemoryApproachSplitFailAggregationRepository()
+        {
+            _db = new InMemoryMOEDatabase();
+        }
+
+        public InMemoryApproachSplitFailAggregationRepository(InMemoryMOEDatabase context )
+        {
+            _db = context;
+        }
+
+        public List<ApproachSplitFailAggregation> GetApproachSplitFailAggregationByVersionIdAndDateRange(int approachId, DateTime start, DateTime end)
+        {
+         
+                var records = (from r in this._db.ApproachSplitFailAggregations
+                    where r.ApproachId == approachId
+                          && r.BinStartTime >= start && r.BinStartTime <= end
+                    select r).ToList();
+
+                return records;
+            
+        }
+
+        public ApproachSplitFailAggregation Add(ApproachSplitFailAggregation priorityAggregation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ApproachSplitFailAggregation priorityAggregation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(ApproachSplitFailAggregation priorityAggregation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
