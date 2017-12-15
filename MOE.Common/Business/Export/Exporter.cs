d302e6706b7eca7db1ee40ef621bf02ac569fbde
Shane Johnson <shanejohnson@utah.gov>
2017-12-15 14:57:00 -0700

    Generic exporter and csvHelper Library

---
+++ MOE.Common/Business/Export/Exporter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+
+namespace MOE.Common.Business.Export
+{
+    public class Exporter
+    {
+        public Exporter(IEnumerable records, string filePath)
+        {
+            using (StreamWriter writer = File.CreateText("filePath"))
+            {
+                var csv = new CsvWriter(writer);
+                csv.WriteRecords(records);
+
+            }
+        }
+    }
+}
