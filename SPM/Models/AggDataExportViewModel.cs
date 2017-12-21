c60d0ac684ce45ff54674e77fb240baca5ec1d5c
Unknown <yxing@utah.gov>
2017-12-21 16:38:53 -0700

    AggregateDataExport new UI from meeting 12/21/2017

--- SPM/Models/AggDataExportViewModel.cs
+++ SPM/Models/AggDataExportViewModel.cs
@@ -25,7 +25,7 @@ namespace SPM.Models
         public virtual List<int> MovementTypeIDs { get; set; }
         public virtual ICollection<MovementType> AllMovementTypes { get; set; }
         public SPM.Controllers.AggregateDataExportController.EnumSeriesGroupBy EnumSeriesGroupBy { get; set; }
-
+        public SPM.Controllers.AggregateDataExportController.EnumAggregationXaxis EnumAggregationXaxis { get; set; }
         public virtual List<int> LaneTypeIDs { get; set; }
         public virtual ICollection<LaneType> AllLaneTypes { get; set; }
         //public List<SelectListItem> AggregateMetricsList { get; set; }
