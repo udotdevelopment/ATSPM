9b4539c997a9eee94bc19f36a3fd4454a10a8a2f
Unknown <yxing@utah.gov>
2018-01-31 10:19:52 -0700

    Auto stash before merge of "CyclesAndPlans" and "origin/CyclesAndPlans"
    Comment out Compile error in Watchdog test. Initialize in AggregateDataExportControllerTest.

--- SPMTests/Controllers/AggregateDataExportControllerTests.cs
+++ SPMTests/Controllers/AggregateDataExportControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MOE.CommonTests.Models;
 using SPM.Controllers;
 using System;
 using System.Collections.Generic;
@@ -11,10 +12,29 @@ namespace SPM.Controllers.Tests
     [TestClass()]
     public class AggregateDataExportControllerTests
     {
+        private InMemoryMOEDatabase _db = new InMemoryMOEDatabase();
+        [TestInitialize]
+        public void Initialize()
+        {
+            _db.ClearTables();
+            _db.PopulateMetricTypes();
+            _db.PopulateSignal();
+            _db.PopulateRoutes();
+            _db.PopulateRouteWithRouteSignals();
+            _db.PopulateRouteSignalsWithPhaseDirection();
+
+            InMemoryRouteRepository routeRepo = new InMemoryRouteRepository(_db);
+            MOE.Common.Models.Repositories.RouteRepositoryFactory.SetApproachRouteRepository(routeRepo);
+        }
         [TestMethod()]
         public void CreateMetricTest()
         {
             int a = 0;
             Assert.IsTrue(a==0);
-        }}
+            int id = 20; //Aggregate Metric
+            var TestController = new AggregateDataExportController();
+            var TestActionResult = TestController.CreateMetric(20);
+            Assert.IsNull(TestActionResult);
+        }
+    }
 }
