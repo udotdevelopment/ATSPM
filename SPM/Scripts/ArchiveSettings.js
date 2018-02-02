768f70a1c0bcbb462803e5623204097e35ffa502
Unknown <yxing@utah.gov>
2018-02-02 14:33:01 -0700

    ArchiveSettings click to show/hide js functions

---
+++ SPM/Scripts/ArchiveSettings.js
@@ -0,0 +1,41 @@
+﻿function OffOptions() {
+    var thisIsOff = document.getElementById("IsOff").checked;
+    if (thisIsOff == true) {
+        $('#DivOff').addClass("hidden");
+    }
+    //else {
+    //    $('#DivOff').removeClass("hidden");
+    //}
+}
+
+function PartitionTablesOptions() {
+    var thisIsPartitionTable = document.getElementById("IsPartitionTables").checked;
+    if (thisIsPartitionTable == true) {
+        $('#DivOff').removeClass("hidden");
+        $('#DivMonthsToRemoveIndex').removeClass("hidden");
+        $('#DivEndTime').addClass("hidden");
+    }
+}
+
+function NonPartitionTablesOptions() {
+    var thisIsNonPartitionTable = document.getElementById("IsNonPartitionTables").checked;
+    if (thisIsNonPartitionTable == true) {
+        $('#DivOff').removeClass("hidden");
+        $('#DivMonthsToRemoveIndex').addClass("hidden");
+        $('#DivEndTime').removeClass("hidden");
+    }
+}
+
+function DeleteOptions() {
+    var thisIsDelete = document.getElementById("IsDelete").checked;
+    if (thisIsDelete == true) {
+        $('#DivMovePath').addClass("hidden");
+    }
+}
+
+function MoveOptions() {
+    var thisIsMove = document.getElementById("IsMove").checked;
+    if (thisIsMove == true) {
+        $('#DivMovePath').removeClass("hidden");
+    }
+}
