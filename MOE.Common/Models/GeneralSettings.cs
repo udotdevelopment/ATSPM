23f0eedf0e647e83b262576b484a7838f5408107
Unknown <yxing@utah.gov>
2018-02-15 14:09:36 -0700

    Auto stash before merge of "CyclesAndPlans" and "origin/CyclesAndPlans"

---
+++ MOE.Common/Models/GeneralSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOE.Common.Models
+{
+
+    public class GeneralSettings : ApplicationSettings
+    {
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Image Path")]
+        public string ImagePath { get; set; }
+        [Display(Name = "Raw Data Count Limit")]
+        public string RawDataCountLimit { get; set; }
+    }
+}
