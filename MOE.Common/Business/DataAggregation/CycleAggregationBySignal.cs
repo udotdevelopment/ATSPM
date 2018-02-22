6c84287d288eda0c3d70f1f994566a2b7180c04e
Unknown <yxing@utah.gov>
2018-02-22 09:45:10 -0700

    ApproachCycleAggregationOptions

---
+++ MOE.Common/Business/DataAggregation/CycleAggregationBySignal.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MOE.Common.Business.WCFServiceLibrary;
+using MOE.Common.Models;
+
+namespace MOE.Common.Business.DataAggregation
+{
+    public class CycleAggregationBySignal : AggregationBySignal
+    {
+        public CycleAggregationBySignal(ApproachCycleAggregationOptions options, Models.Signal signal) : base(
+            options, signal)
+        {
+            ApproachCycleures = new List<CycleAggregationByApproach>();
+            GetApproachCycleAggregationContainersForAllApporaches(options, signal);
+            LoadBins(null, null);
+        }
+
+        public CycleAggregationBySignal(ApproachCycleAggregationOptions options, Models.Signal signal,
+            int phaseNumber) : base(options, signal)
+        {
+            ApproachCycleures = new List<CycleAggregationByApproach>();
+            foreach (var approach in signal.Approaches)
+                if (approach.ProtectedPhaseNumber == phaseNumber)
+                {
+                    ApproachCycleures.Add(
+                        new CycleAggregationByApproach(approach, options.TimeOptions, options.StartDate,
+                            options.EndDate,
+                            true, options.SelectedAggregatedDataType));
+                    if (approach.PermissivePhaseNumber != null && approach.PermissivePhaseNumber == phaseNumber)
+                        ApproachCycleures.Add(
+                            new CycleAggregationByApproach(approach, options.TimeOptions, options.StartDate,
+                                options.EndDate,
+                                false, options.SelectedAggregatedDataType));
+                }
+            LoadBins(null, null);
+        }
+
+        public CycleAggregationBySignal(ApproachCycleAggregationOptions options, Models.Signal signal,
+            DirectionType direction) : base(options, signal)
+        {
+            ApproachCycleures = new List<CycleAggregationByApproach>();
+            foreach (var approach in signal.Approaches)
+                if (approach.DirectionType.DirectionTypeID == direction.DirectionTypeID)
+                {
+                    ApproachCycleures.Add(
+                        new CycleAggregationByApproach(approach, options.TimeOptions, options.StartDate,
+                            options.EndDate,
+                            true, options.SelectedAggregatedDataType));
+                    if (approach.PermissivePhaseNumber != null)
+                        ApproachCycleures.Add(
+                            new CycleAggregationByApproach(approach, options.TimeOptions, options.StartDate,
+                                options.EndDate,
+                                false, options.SelectedAggregatedDataType));
+                }
+            LoadBins(null, null);
+        }
+
+        public List<CycleAggregationByApproach> ApproachCycleures { get; }
+
+        protected override void LoadBins(SignalAggregationMetricOptions options, Models.Signal signal)
+        {
+            for (var i = 0; i < BinsContainers.Count; i++)
+            for (var binIndex = 0; binIndex < BinsContainers[i].Bins.Count; binIndex++)
+            {
+                var bin = BinsContainers[i].Bins[binIndex];
+                foreach (var approachCycleAggregationContainer in ApproachCycleures)
+                    bin.Sum += approachCycleAggregationContainer.BinsContainers[i].Bins[binIndex].Sum;
+                bin.Average = ApproachCycleures.Count > 0 ? bin.Sum / ApproachCycleures.Count : 0;
+            }
+        }
+
+        private void GetApproachCycleAggregationContainersForAllApporaches(
+            ApproachCycleAggregationOptions options, Models.Signal signal)
+        {
+            foreach (var approach in signal.Approaches)
+            {
+                ApproachCycleures.Add(
+                    new CycleAggregationByApproach(approach, options.TimeOptions, options.StartDate,
+                        options.EndDate,
+                        true, options.SelectedAggregatedDataType));
+                if (approach.PermissivePhaseNumber != null)
+                    ApproachCycleures.Add(
+                        new CycleAggregationByApproach(approach, options.TimeOptions, options.StartDate,
+                            options.EndDate,
+                            false, options.SelectedAggregatedDataType));
+            }
+        }
+
+
+        public int GetCyclesByDirection(DirectionType direction)
+        {
+            var splitFails = 0;
+            if (ApproachCycleures != null)
+                splitFails = ApproachCycleures
+                    .Where(a => a.Approach.DirectionType.DirectionTypeID == direction.DirectionTypeID)
+                    .Sum(a => a.BinsContainers.FirstOrDefault().SumValue);
+            return splitFails;
+        }
+
+        public int GetAverageCyclesByDirection(DirectionType direction)
+        {
+            var approachCycleuresByDirection = ApproachCycleures
+                .Where(a => a.Approach.DirectionType.DirectionTypeID == direction.DirectionTypeID);
+            var splitFails = 0;
+            if (approachCycleuresByDirection.Any())
+                splitFails = Convert.ToInt32(Math.Round(approachCycleuresByDirection
+                    .Average(a => a.BinsContainers.FirstOrDefault().SumValue)));
+            return splitFails;
+        }
+    }
+}
---
+++ MOE.Common/Business/WCFServiceLibrary/ApproachCycleAggregationOptions.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Concurrent;
+using MOE.Common.Models;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using MOE.Common.Business.Bins;
+using MOE.Common.Business.DataAggregation;
+using MOE.Common.Business.FilterExtensions;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MOE.Common.Business.WCFServiceLibrary
+{
+
+    [DataContract]
+    public class ApproachCycleAggregationOptions: ApproachAggregationMetricOptions
+    {
+        public override string ChartTitle
+        {
+            get
+            {
+                string chartTitle;
+                chartTitle = "AggregationChart\n";
+                chartTitle += TimeOptions.Start.ToString();
+                if (TimeOptions.End > TimeOptions.Start)
+                    chartTitle += " to " + TimeOptions.End.ToString() + "\n";
+                if (TimeOptions.DaysOfWeek != null)
+                {
+                    foreach (var dayOfWeek in TimeOptions.DaysOfWeek)
+                    {
+                        chartTitle += dayOfWeek.ToString() + " ";
+                    }
+                }
+                if (TimeOptions.TimeOfDayStartHour != null && TimeOptions.TimeOfDayStartMinute != null &&
+                    TimeOptions.TimeOfDayEndHour != null && TimeOptions.TimeOfDayEndMinute != null)
+                {
+                    chartTitle += "Limited to: " + new TimeSpan(0, TimeOptions.TimeOfDayStartHour.Value, TimeOptions.TimeOfDayStartMinute.Value, 0)
+                                      .ToString() + " to " + new TimeSpan(0, TimeOptions.TimeOfDayEndHour.Value,
+                                      TimeOptions.TimeOfDayEndMinute.Value, 0).ToString() + "\n";
+                }
+                chartTitle += TimeOptions.SelectedBinSize.ToString() + " bins ";
+                chartTitle += SelectedXAxisType.ToString() + " Aggregation ";
+                chartTitle += SelectedAggregationType.ToString();
+                return chartTitle;
+            }
+        }
+        
+
+
+        public  ApproachCycleAggregationOptions()
+        {
+            MetricTypeID = 20;
+            AggregatedDataTypes = new List<AggregatedDataType>();
+            AggregatedDataTypes.Add(new AggregatedDataType { Id = 0, DataName = "Cycles" });
+            AggregatedDataTypes.Add(new AggregatedDataType { Id = 1, DataName = "GapOuts" });
+            AggregatedDataTypes.Add(new AggregatedDataType { Id = 2, DataName = "ForceOffs" });
+            AggregatedDataTypes.Add(new AggregatedDataType { Id = 3, DataName = "MaxOuts" });
+
+        }
+
+
+
+        protected override int GetAverageByPhaseNumber(Models.Signal signal, int phaseNumber)
+        {
+            CycleAggregationBySignal splitFailAggregationBySignal =
+                new CycleAggregationBySignal(this, signal);
+            return splitFailAggregationBySignal.Average;
+        }
+
+        protected override int GetSumByPhaseNumber(Models.Signal signal, int phaseNumber)
+        {
+            CycleAggregationBySignal splitFailAggregationBySignal =
+                new CycleAggregationBySignal(this, signal);
+            return splitFailAggregationBySignal.Total;
+        }
+
+        protected override int GetAverageByDirection(Models.Signal signal, DirectionType direction)
+        {
+            CycleAggregationBySignal splitFailAggregationBySignal =
+                new CycleAggregationBySignal(this, signal, direction);
+            return splitFailAggregationBySignal.Average;
+        }
+
+        protected override int GetSumByDirection(Models.Signal signal, DirectionType direction)
+        {
+            CycleAggregationBySignal splitFailAggregationBySignal =
+                new CycleAggregationBySignal(this, signal, direction);
+            return splitFailAggregationBySignal.Total;
+        }
+
+        protected override List<BinsContainer> GetBinsContainersBySignal(Models.Signal signal)
+        {
+            CycleAggregationBySignal splitFailAggregationBySignal = new CycleAggregationBySignal(this, signal);
+            return splitFailAggregationBySignal.BinsContainers;
+        }
+
+        protected override List<BinsContainer> GetBinsContainersByDirection(DirectionType directionType, Models.Signal signal)
+        {
+            CycleAggregationBySignal splitFailAggregationBySignal =
+                new CycleAggregationBySignal(this, signal, directionType);
+            return splitFailAggregationBySignal.BinsContainers;
+        }
+
+        protected override List<BinsContainer> GetBinsContainersByPhaseNumber(Models.Signal signal, int phaseNumber)
+        {
+            CycleAggregationBySignal splitFailAggregationBySignal =
+                new CycleAggregationBySignal(this, signal, phaseNumber);
+            return splitFailAggregationBySignal.BinsContainers;
+        }
+
+        protected override List<BinsContainer> GetBinsContainersByRoute(List<Models.Signal> signals)
+        {
+            ConcurrentBag<CycleAggregationBySignal> aggregations = new ConcurrentBag<CycleAggregationBySignal>();
+            Parallel.ForEach(signals, signal =>
+            {
+                aggregations.Add(new CycleAggregationBySignal(this, signal));
+            });
+            var binsContainers = BinFactory.GetBins(TimeOptions);
+            foreach (var splitFailAggregationBySignal in aggregations)
+            {
+                for (int i = 0; i < binsContainers.Count; i++)
+                {
+                    for (var binIndex = 0; binIndex < binsContainers[i].Bins.Count; binIndex++)
+                    {
+                        var bin = binsContainers[i].Bins[binIndex];
+                        bin.Sum += splitFailAggregationBySignal.BinsContainers[i].Bins[binIndex].Sum;
+                        bin.Average = Convert.ToInt32(Math.Round((double)(bin.Sum / signals.Count)));
+                    }
+                }
+            }
+            return binsContainers;
+        }
+
+        public override string YAxisTitle
+        {
+            get
+            {
+                return SelectedAggregationType.ToString() + " of Split Fail " + Regex.Replace(SelectedAggregatedDataType.ToString(),
+                               @"(\B[A-Z]+?(?=[A-Z][^A-Z])|\B[A-Z]+?(?=[^A-Z]))", " $1").ToString() + " " + TimeOptions.SelectedBinSize.ToString() + " bins";
+            }
+        }
+
+
+        protected override List<BinsContainer> GetBinsContainersByApproach(Models.Approach approach, bool getprotectedPhase)
+        {
+            CycleAggregationByApproach approachCycleAggregationContainer = new CycleAggregationByApproach(approach, TimeOptions, StartDate, EndDate,
+                getprotectedPhase, SelectedAggregatedDataType);
+            return approachCycleAggregationContainer.BinsContainers;
+        }
+        
+    }
+}
