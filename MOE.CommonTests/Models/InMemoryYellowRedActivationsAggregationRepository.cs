b73dfc61fa4f821a4fbb32156e12b697c6978571
Unknown <yxing@utah.gov>
2018-02-26 18:00:48 -0700

    Run Chart Generation tests for Approach Yellow Red Activation Aggregation Chart

---
+++ MOE.CommonTests/Models/InMemoryYellowRedActivationsAggregationRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MOE.Common.Business.DataAggregation;
+using MOE.Common.Models;
+using MOE.Common.Models.Repositories;
+
+namespace MOE.CommonTests.Models
+{
+    public partial class InMemoryYellowRedActivationsAggregationByApproachRepository: IApproachYellowRedActivationsAggregationRepository
+    {
+        private InMemoryMOEDatabase _db;
+
+        public InMemoryYellowRedActivationsAggregationByApproachRepository()
+        {
+            _db = new InMemoryMOEDatabase();
+        }
+
+        public InMemoryYellowRedActivationsAggregationByApproachRepository(InMemoryMOEDatabase context )
+        {
+            _db = context;
+        }
+
+        public int GetApproachYellowRedActivationsCountAggregationByApproachIdAndDateRange(int versionId, DateTime start,
+            DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public YellowRedActivationsAggregationByApproach Add(YellowRedActivationsAggregationByApproach priorityAggregation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(YellowRedActivationsAggregationByApproach priorityAggregation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(YellowRedActivationsAggregationByApproach priorityAggregation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ApproachYellowRedActivationAggregation> GetApproachYellowRedActivationssAggregationByApproachIdAndDateRange(int approachId, DateTime startDate, DateTime endDate, bool getProtectedPhase)
+        {
+            return _db.ApproachYellowRedActivationAggregations.Where(r => r.ApproachId == approachId
+                                                                && r.BinStartTime >= startDate &&
+                                                                r.BinStartTime < endDate && r.IsProtectedPhase == getProtectedPhase).ToList();
+        }
+    }
+}
