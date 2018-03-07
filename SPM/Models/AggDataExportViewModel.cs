2c3b4b9f75296a30249e90c092a2e20e06460db8
Unknown <yxing@utah.gov>
2018-03-07 12:48:10 -0700

    Match ChartName (configuration.cs) with the metrics that have been implemented under MOE.Common\Business\WCFServiceLibrary

--- SPM/Models/AggDataExportViewModel.cs
+++ SPM/Models/AggDataExportViewModel.cs
@@ -220,7 +220,7 @@ namespace SPM.Models
                     case 24:
                         AggregatedDataTypes = new SignalPriorityAggregationOptions().AggregatedDataTypes;
                         break;
-                    case 16:
+                    case 26:
                         AggregatedDataTypes = new ApproachYellowRedActivationsAggregationOptions().AggregatedDataTypes;
                         break;
                     default:
