34cc6bcbb858b5583b8b379a82dbf6f3a2548ca7
Unknown <dlowe@utah.gov>
2018-03-09 12:32:34 -0700

    Auto stash before merge of "CyclesAndPlans" and "origin/CyclesAndPlans"

--- MOE.CommonTests/Business/SplitFail/SplitFailPhaseTests.cs
+++ MOE.CommonTests/Business/SplitFail/SplitFailPhaseTests.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MOE.Common.Business.WCFServiceLibrary;
+using MOE.Common.Models.Repositories;
 using MOE.CommonTests.Helpers;
 using MOE.CommonTests.Models;
 
@@ -19,31 +20,38 @@ namespace MOE.Common.Business.SplitFail.Tests
         {
             AddControllerEventLogsToInMemoryDb.LoadControllerEventLogs(inMemoryMoeDatabase);
             inMemoryMoeDatabase.AddTestSignalForSplitFailTest();
+            XMLToListImporter
+            //SignalsRepositoryFactory.SetSignalsRepository(new InMemorySignalsRepository(inMemoryMoeDatabase));
+            //ControllerEventLogRepositoryFactory.SetRepository(new InMemoryControllerEventLogRepository(inMemoryMoeDatabase));
         }
         [TestMethod()]
         public void SplitFailPhaseTest()
         {
-            SplitFailOptions splitFailOptions = new SplitFailOptions{ EndDate = new DateTime(2017, 10,17, 17, 11, 1 ), StartDate = new DateTime(2017, 10, 17, 17, 8, 13), FirstSecondsOfRed = 1, SignalID = "7185", MetricTypeID = 12, ShowAvgLines = true, ShowPercentFailLines = true, ShowFailLines = true, Y2AxisMax = null, YAxisMin = 0, Y2AxisMin = 0, YAxisMax = null};
+            SplitFailOptions splitFailOptions = new SplitFailOptions{StartDate = new DateTime(2017, 10, 17, 17, 0, 0), EndDate = new DateTime(2017, 10,17, 17, 11, 1 ),  FirstSecondsOfRed = 5, SignalID = "7185", MetricTypeID = 12, ShowAvgLines = true, ShowPercentFailLines = true, ShowFailLines = true, Y2AxisMax = null, YAxisMin = 0, Y2AxisMin = 0, YAxisMax = null};
             var signal = inMemoryMoeDatabase.Signals.FirstOrDefault();
             SplitFailPhase splitFailPhase = new SplitFailPhase(signal.Approaches.FirstOrDefault(), splitFailOptions, true);
-            Assert.IsTrue(splitFailPhase.Cycles[0].StartTime == new DateTime(2017,10, 17, 17, 9, 33));
-            Assert.IsTrue(splitFailPhase.Cycles[0].YellowEvent == new DateTime(2017, 10, 17, 17, 10, 09));
-            Assert.IsTrue(splitFailPhase.Cycles[0].RedEvent == new DateTime(2017, 10, 17, 17, 10, 13));
-            Assert.IsTrue(splitFailPhase.Cycles[0].ActivationsDuringGreen.Count == 2);
-            Assert.IsTrue(splitFailPhase.Cycles[0].ActivationsDuringGreen[0].DetectorOn == new DateTime(2017, 10, 17, 17, 1, 59, 5));
-            Assert.IsTrue(splitFailPhase.Cycles[0].ActivationsDuringGreen[0].DetectorOff == new DateTime(2017, 10, 17, 17, 9, 43, 8));
-            Assert.IsTrue(splitFailPhase.Cycles[0].ActivationsDuringGreen[0].DurationInMilliseconds == 10800);
-            Assert.IsTrue(splitFailPhase.Cycles[0].ActivationsDuringGreen[1].DetectorOn == new DateTime(2017, 10, 17, 17, 1, 55, 4));
-            Assert.IsTrue(splitFailPhase.Cycles[0].ActivationsDuringGreen[1].DetectorOff == new DateTime(2017, 10, 17, 17, 9, 06, 3));
-            Assert.IsTrue(splitFailPhase.Cycles[0].ActivationsDuringGreen[1].DurationInMilliseconds == 10900);
-            Assert.IsTrue(splitFailPhase.Cycles[0].ActivationsDuringRed.Count == 1);
-            Assert.IsTrue(splitFailPhase.Cycles[0].ActivationsDuringRed[0].DetectorOn == new DateTime(2017, 10, 17, 17, 10, 9, 2));
-            Assert.IsTrue(splitFailPhase.Cycles[0].ActivationsDuringRed[0].DetectorOff == new DateTime(2017, 10, 17, 17, 10, 13, 3));
-            Assert.IsTrue(splitFailPhase.Cycles[0].ActivationsDuringRed[0].DurationInMilliseconds == 300);
-            Assert.IsTrue(splitFailPhase.Cycles[0].GreenOccupancyPercent == 60.0);
-            Assert.IsTrue(splitFailPhase.Cycles[0].GreenOccupancyTimeInMilliseconds == 3600.0);
-            Assert.IsTrue(splitFailPhase.Cycles[0].RedOccupancyPercent == 6.0);
-            Assert.IsTrue(splitFailPhase.Cycles[0].RedOccupancyTimeInMilliseconds == 300.0);
+            Assert.IsTrue(splitFailPhase.Cycles[4].StartTime == new DateTime(2017,10, 17, 17, 9, 33));
+            Assert.IsTrue(splitFailPhase.Cycles[4].YellowEvent == new DateTime(2017, 10, 17, 17, 10, 09));
+            Assert.IsTrue(splitFailPhase.Cycles[4].RedEvent == new DateTime(2017, 10, 17, 17, 10, 13));
+            Assert.IsTrue(splitFailPhase.Cycles[4].ActivationsDuringGreen.Count == 2);
+            DateTime date1 = new DateTime(2017, 10, 17, 17, 1, 59, 500);
+            Assert.IsTrue(splitFailPhase.Cycles[4].ActivationsDuringGreen[0].DetectorOn == date1);
+            Assert.IsTrue(splitFailPhase.Cycles[4].ActivationsDuringGreen[0].DetectorOff == new DateTime(2017, 10, 17, 17, 9, 43, 800));
+            Assert.IsTrue(splitFailPhase.Cycles[4].ActivationsDuringGreen[0].DurationInMilliseconds == 464300);
+            Assert.IsTrue(splitFailPhase.Cycles[4].ActivationsDuringGreen[1].DetectorOn == new DateTime(2017, 10, 17, 17, 9, 55, 400));
+            Assert.IsTrue(splitFailPhase.Cycles[4].ActivationsDuringGreen[1].DetectorOff == new DateTime(2017, 10, 17, 17, 10, 6, 300));
+            Assert.IsTrue(splitFailPhase.Cycles[4].ActivationsDuringGreen[1].DurationInMilliseconds == 10900);
+            Assert.IsTrue(splitFailPhase.Cycles[4].ActivationsDuringRed.Count == 1);
+            Assert.IsTrue(splitFailPhase.Cycles[4].ActivationsDuringRed[0].DetectorOn == new DateTime(2017, 10, 17, 17, 10, 9, 200));
+            Assert.IsTrue(splitFailPhase.Cycles[4].ActivationsDuringRed[0].DetectorOff == new DateTime(2017, 10, 17, 17, 10, 13, 300));
+            Assert.IsTrue(splitFailPhase.Cycles[4].ActivationsDuringRed[0].DurationInMilliseconds == 4100);
+            Assert.IsTrue(splitFailPhase.Cycles[4].GreenOccupancyTimeInMilliseconds == 21700.0);
+            Assert.IsTrue(splitFailPhase.Cycles[4].TotalGreenTimeMilliseconds == 36000.0);
+            Assert.IsTrue(splitFailPhase.Cycles[4].RedOccupancyTimeInMilliseconds == 300.0);
+            Assert.IsTrue(splitFailPhase.Cycles[4].RedOccupancyPercent == 6.0);
+            Assert.IsTrue(splitFailPhase.Cycles[4].FirstSecondsOfRed == 5); 
+            Assert.IsTrue(Math.Round(splitFailPhase.Cycles[4].GreenOccupancyPercent) == 60.0);
+            splitFailOptions.CreateMetric();
         }
     }
 }
