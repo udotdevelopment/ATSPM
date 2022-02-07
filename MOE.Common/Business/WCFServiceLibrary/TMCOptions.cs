0b1f2f5b27d27f23703d369c2e1c34c522a1ffc8
Nuzhat Azra <nazra@avenueconsultants.com>
2022-02-07 16:12:27 -0700

    Fix for Bug 4 - TMC Charts will now be created for Lane-by-Lane count detectors if they are marked as Pedestrian, Bus, Light rail, or HOV.

--- MOE.Common/Business/WCFServiceLibrary/TMCOptions.cs
+++ MOE.Common/Business/WCFServiceLibrary/TMCOptions.cs
@@ -97,8 +97,10 @@ namespace MOE.Common.Business.WCFServiceLibrary
             CreateLaneTypeCharts(signal, "Vehicle", laneTypes, movementTypes, directions, plans, TmcInfo);
             CreateLaneTypeCharts(signal, "Exit", laneTypes, movementTypes, directions, plans, TmcInfo);
             CreateLaneTypeCharts(signal, "Bike", laneTypes, movementTypes, directions, plans, TmcInfo);
-
-
+            CreateLaneTypeCharts(signal, "Pedestrian", laneTypes, movementTypes, directions, plans, TmcInfo);
+            CreateLaneTypeCharts(signal, "Bus", laneTypes, movementTypes, directions, plans, TmcInfo);
+            CreateLaneTypeCharts(signal, "Light Rail Transit", laneTypes, movementTypes, directions, plans, TmcInfo);
+            CreateLaneTypeCharts(signal, "High Occupancy Vehicle", laneTypes, movementTypes, directions, plans, TmcInfo);
             return TmcInfo;
         }
 
