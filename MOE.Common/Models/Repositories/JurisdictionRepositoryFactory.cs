4966c541b646453286ed9b82cff025da2d4c2a2a
Nuzhat Azra <nazra@avenueconsultants.com>
2022-03-21 12:20:00 -0700

    factory repository for new Jurisdiction tab

---
+++ MOE.Common/Models/Repositories/JurisdictionRepositoryFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace MOE.Common.Models.Repositories
+{
+    public class JurisdictionRepositoryFactory
+    {
+        private static IJurisdictionRepository approachJurisdictionRepository;
+
+        public static IJurisdictionRepository Create()
+        {
+            if (approachJurisdictionRepository != null)
+                return approachJurisdictionRepository;
+            return new JurisdictionRepository();
+        }
+
+        public static void SetApproachRouteRepository(IJurisdictionRepository newRepository)
+        {
+            approachJurisdictionRepository = newRepository;
+        }
+    }
+}
