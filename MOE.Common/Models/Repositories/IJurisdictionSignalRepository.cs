f8480d9c5f5cc0a735ebdd3dfaf6a05e672a13b8
Nuzhat Azra <nazra@avenueconsultants.com>
2022-03-21 11:59:19 -0700

    IJurisdictionSignal for new Jurisdiction Tab

---
+++ MOE.Common/Models/Repositories/IJurisdictionSignalRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MOE.Common.Models.Repositories
+{
+    public interface IJurisdictionSignalsRepository
+    {
+        List<JurisdictionSignal> GetAllJurisdictionsDetails();
+        List<JurisdictionSignal> GetByJurisdictionID(int JurisdictionID);
+        void DeleteByJurisdictionID(int JurisdictionID);
+        void DeleteById(int id);
+        void UpdateByJurisdictionAndApproachID(int JurisdictionID, string signalId, int newOrderNumber);
+        void Add(JurisdictionSignal newJurisdictionDetail);
+        JurisdictionSignal GetByJurisdictionSignalId(int id);
+        void MoveJurisdictionSignalUp(int JurisdictionId, int JurisdictionSignalId);
+        void MoveJurisdictionSignalDown(int JurisdictionId, int JurisdictionSignalId);
+    }
+}
