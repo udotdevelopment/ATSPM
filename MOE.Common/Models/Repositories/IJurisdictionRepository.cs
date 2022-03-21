c3c8445bc5dd55d535a6da61deecb70192cbe829
Nuzhat Azra <nazra@avenueconsultants.com>
2022-03-21 11:58:26 -0700

    new IJurisdiction for new Jurisdiction Tab

---
+++ MOE.Common/Models/Repositories/IJurisdictionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MOE.Common.Models.Repositories
+{
+    public interface IJurisdictionRepository
+    {
+        List<Jurisdiction> GetAllJurisdictions();
+        Jurisdiction GetJurisdictionByID(int jurisdictionID);
+        Jurisdiction GetJurisdictionByName(string jurisdictionName);
+        void DeleteByID(int jurisdictionID);
+        void Update(Jurisdiction jurisdiction);
+        void Add(Jurisdiction newJurisdiction);
+        Jurisdiction GetJurisdictionByIDAndDate(int jurisdictionId, DateTime startDate);
+    }
+}
