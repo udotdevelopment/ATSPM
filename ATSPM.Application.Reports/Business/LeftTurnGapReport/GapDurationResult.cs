9234173167de713d93c287d8f5d6d6a905c3ae84
dlowe <dlowe@avenueconsultants.com>
2022-04-30 09:41:13 -0600

    Fixes for Gap Duration Calculation

--- ATSPM.Application.Reports/Business/LeftTurnGapReport/GapDurationResult.cs
+++ ATSPM.Application.Reports/Business/LeftTurnGapReport/GapDurationResult.cs
@@ -5,13 +5,12 @@ using System.Threading.Tasks;
 
 namespace ATSPM.Application.Reports.Business.LeftTurnGapReport
 {
-    public class GapOutResult
+    public class GapDurationResult
     {
-        public double GapOutPercent { get; set; }
+        public double GapDurationPercent { get; set; }
         public double Capacity { get; set; }    
         public double Demand { get; set; }
         public Dictionary<DateTime, Double> AcceptableGaps { get; set; }
-        public Dictionary<DateTime, Double> DetectorCount { get; set; }
         public String Direction { get; set; }
         public string OpposingDirection { get; internal set; }
     }
