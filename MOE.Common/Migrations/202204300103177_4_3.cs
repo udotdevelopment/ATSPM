09864fa67d38301cce76cc35084c9b13ecd9e3ca
dlowe <dlowe@avenueconsultants.com>
2022-05-05 09:27:05 -0600

    combine migrations

--- MOE.Common/Migrations/202204300103177_4_3.cs
+++ MOE.Common/Migrations/202204300103177_4_3.cs
@@ -8,6 +8,15 @@
         public override void Up()
         {
             CreateTable(
+                "dbo.Areas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        AreaName = c.String(maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
                 "dbo.Jurisdictions",
                 c => new
                     {
@@ -29,12 +38,24 @@
                     })
                 .PrimaryKey(t => new { t.Measure, t.OptionName });
             
-            AddColumn("dbo.Signals", "JurisdictionId", c => c.Int(nullable: false, defaultValue:1));
+            CreateTable(
+                "dbo.AreaSignals",
+                c => new
+                    {
+                        Area_Id = c.Int(nullable: false),
+                        Signal_VersionID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Area_Id, t.Signal_VersionID })
+                .ForeignKey("dbo.Areas", t => t.Area_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Signals", t => t.Signal_VersionID, cascadeDelete: true)
+                .Index(t => t.Area_Id)
+                .Index(t => t.Signal_VersionID);
+            
+            AddColumn("dbo.Signals", "JurisdictionId", c => c.Int(nullable: false));
             AddColumn("dbo.Signals", "Pedsare1to1", c => c.Boolean(nullable: false));
             AddColumn("dbo.Approaches", "PedestrianPhaseNumber", c => c.Int());
             AddColumn("dbo.Approaches", "IsPedestrianPhaseOverlap", c => c.Boolean(nullable: false));
             AddColumn("dbo.Approaches", "PedestrianDetectors", c => c.String());
-            Sql("insert into Jurisdictions(JurisdictionName) values ('Default')");
             CreateIndex("dbo.Signals", "JurisdictionId");
             AddForeignKey("dbo.Signals", "JurisdictionId", "dbo.Jurisdictions", "Id");
         }
@@ -42,14 +63,20 @@
         public override void Down()
         {
             DropForeignKey("dbo.Signals", "JurisdictionId", "dbo.Jurisdictions");
+            DropForeignKey("dbo.AreaSignals", "Signal_VersionID", "dbo.Signals");
+            DropForeignKey("dbo.AreaSignals", "Area_Id", "dbo.Areas");
+            DropIndex("dbo.AreaSignals", new[] { "Signal_VersionID" });
+            DropIndex("dbo.AreaSignals", new[] { "Area_Id" });
             DropIndex("dbo.Signals", new[] { "JurisdictionId" });
             DropColumn("dbo.Approaches", "PedestrianDetectors");
             DropColumn("dbo.Approaches", "IsPedestrianPhaseOverlap");
             DropColumn("dbo.Approaches", "PedestrianPhaseNumber");
             DropColumn("dbo.Signals", "Pedsare1to1");
             DropColumn("dbo.Signals", "JurisdictionId");
+            DropTable("dbo.AreaSignals");
             DropTable("dbo.MeasuresDefaults");
             DropTable("dbo.Jurisdictions");
+            DropTable("dbo.Areas");
         }
     }
 }
